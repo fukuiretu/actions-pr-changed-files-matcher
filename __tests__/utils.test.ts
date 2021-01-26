f1311d8e4af0bf12973d06d6c7a2fc063fb5e621
fukuiretu <fukuiretu@gmail.com>
2021-01-27 00:17:02 +0900

    tmp

---
+++ __tests__/utils.test.ts
@@ -0,0 +1,24 @@
+import * as utils from '../src/util'
+import {Minimatch, IMinimatch} from 'minimatch'
+
+describe('#isMatch', () => {
+  test('完全一致', async () => {
+    const targets = [new Minimatch('src/hoge.rb')]
+    expect(utils.isMatch('src/hoge.rb', targets)).toBe(true)
+  })
+
+  test('部分一致', async () => {
+    const targets = [new Minimatch('src/ho*.rb')]
+    expect(utils.isMatch('src/hoge.rb', targets)).toBe(true)
+  })
+
+  test('特定のディレクトリ配下', async () => {
+    const targets = [new Minimatch('src/**/*')]
+    expect(utils.isMatch('src/app/hoge.rb', targets)).toBe(true)
+  })
+
+  test('特定の拡張子', async () => {
+    const targets = [new Minimatch('**/*.rb')]
+    expect(utils.isMatch('src/app/models/hoge.rb', targets)).toBe(true)
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,45 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import S3 from 'aws-sdk/clients/s3'
+import * as github from '@actions/github'
+import * as util from './util'
+import * as yaml from 'js-yaml'
+import {IMinimatch} from 'minimatch'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    let result = false
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const prNumber = util.getPrNumber()
+    if (!prNumber) {
+      // TODO
+      core.setOutput('result', result)
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const s3 = new S3({
+      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
+      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
+      region: process.env['AWS_DEFAULT_REGION']
+    })
+    const body = util.fetchBody(
+      s3,
+      core.getInput('s3-bucket') || '',
+      core.getInput('s3-key') || ''
+    )
+    const matchers: IMinimatch[] = util.getMatchers(yaml.safeLoad(body))
+
+    const token: string = core.getInput('github-token')
+    const client: any = github.getOctokit(token, {log: console})
+
+    const changedFiles = await util.getChangedFiles(client, prNumber)
+    for (const changedFile of changedFiles) {
+      if (util.isMatch(changedFile, matchers)) {
+        result = true
+        break
+      }
+    }
+
+    core.setOutput('result', result)
   } catch (error) {
     core.setFailed(error.message)
   }
