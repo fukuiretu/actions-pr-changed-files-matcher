d14fc9655755fd8b8af237f73b77f9478e408db7
fukuiretu <fukuiretu@gmail.com>
2021-03-01 01:34:58 +0900

    tweak

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,11 @@ async function run(): Promise<void> {
       return
     }
 
-    AWS.config.update({
+    const s3 = new AWS.S3({
       accessKeyId: process.env['AWS_ACCESS_KEY_ID'] || '',
       secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'] || '',
       region: process.env['AWS_DEFAULT_REGION'] || ''
     })
-
-    const s3 = new AWS.S3()
     const body = util.fetchBody(
       s3,
       core.getInput('s3-bucket') || '',
